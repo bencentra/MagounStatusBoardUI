158406993c8cba532a0ea49bec0bd3be76bbd55c
Ben <bcentra@constantcontact.com>
2017-10-08 12:42:42 -0400

    example code for redline times (too bad there are no results right now)

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const API_KEY = 'wX9NwuHnZU2ToO7GmGR9uw';
+const API_KEY = 'zwnfedkyJ0m-ITfTerDfqg';
 const BASE_URL = 'http://realtime.mbta.com/developer/api/v2/';
 const FORMAT = 'json'
 
@@ -27,11 +27,34 @@ async function getJSON(url) {
   const response = await fetch(url);
   const json = await response.json();
   return json;
-};
+}
+
+async function getRedlineTimes() {
+  const json = await getJSON(ENDPOINTS.predictionsbystop(STOPS.alewife));
+  const subway = json.mode.filter((mode) => mode.mode_name === 'Subway');
+  if (subway.length === 0) throw new Error('no subway found');
+  const outbound = subway[0].route.reduce((prev, curr) => {
+    const southbound = curr.direction.filter((direction) => direction.direction_name === 'Southbound');
+    if (southbound.length === 1) prev.push(southbound[0]);
+    return prev;
+  }, []);
+  if (outbound.length === 0) throw new Error('no outbound direction found');
+  const trips = outbound[0].trip;
+  if (trips.length === 0) throw new Error('no trips found');
+  return trips;
+}
 
 // Do some things
-getJSON(ENDPOINTS.stopsbyroute(ROUTES.redline)).then((json) => console.log('stopsbyroute - redline', json));
-getJSON(ENDPOINTS.stopsbyroute(ROUTES.bus)).then((json) => console.log('stopsbyroute - bus', json));
+// getJSON(ENDPOINTS.stopsbyroute(ROUTES.redline)).then((json) => console.log('stopsbyroute - redline', json));
+// getJSON(ENDPOINTS.stopsbyroute(ROUTES.bus)).then((json) => console.log('stopsbyroute - bus', json));
 getJSON(ENDPOINTS.predictionsbystop(STOPS.alewife)).then((json) => console.log('predictionsbystop - alewife', json));
-getJSON(ENDPOINTS.predictionsbystop(STOPS.magoun)).then((json) => console.log('predictionsbystop - magoun', json));
-getJSON(ENDPOINTS.predictionsbystop(STOPS.gladstone)).then((json) => console.log('predictionsbystop - gladstone', json));
+// getJSON(ENDPOINTS.predictionsbystop(STOPS.magoun)).then((json) => console.log('predictionsbystop - magoun', json));
+// getJSON(ENDPOINTS.predictionsbystop(STOPS.gladstone)).then((json) => console.log('predictionsbystop - gladstone', json));
+
+getRedlineTimes()
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error('redline error:', error.message);
+  });
